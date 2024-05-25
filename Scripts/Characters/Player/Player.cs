75f5bb3d0f50af1a2a3248dabfa8c6ab899a45b7
CJ Davis <csdj92@gmail.com>
2024-05-25 15:05:11 -0400

    updates

--- Scripts/Characters/Player/Player.cs
+++ Scripts/Characters/Player/Player.cs
@@ -4,15 +4,10 @@ using System;
 public partial class Player : CharacterBody3D
 {
     [ExportGroup("Required Nodes")]
-    [Export] private AnimationPlayer animationPlayerNode;
-    [Export] private Sprite3D spriteNode;
-    private Vector2 direction = Vector2.Zero;
-
-    public override void _Ready()
-    {
-        animationPlayerNode.Play(GameConstants.ANIM_IDLE);
-    }
-
+    [Export] public AnimationPlayer animationPlayerNode;
+    [Export] public Sprite3D spriteNode;
+    public Vector2 direction = Vector2.Zero;
+    [Export] public StateMachine stateMachineNode;
     public override void _PhysicsProcess(double delta)
     {
         Velocity = new Vector3(direction.X, 0, direction.Y) * 5;
@@ -25,7 +20,6 @@ public partial class Player : CharacterBody3D
     public override void _Input(InputEvent @event)
     {
         direction = Input.GetVector(GameConstants.INPUT_MOVE_LEFT, GameConstants.INPUT_MOVE_RIGHT, GameConstants.INPUT_MOVE_FORWARD, GameConstants.INPUT_MOVE_DOWN);
-        animationPlayerNode.Play(direction != Vector2.Zero ? GameConstants.ANIM_MOVE : GameConstants.ANIM_IDLE);
     }
 
     private void flip()
---
+++ Scripts/Characters/StateMachine.cs
@@ -0,0 +1,34 @@
+using Godot;
+using System;
+
+public partial class StateMachine : Node
+{
+    [Export] private Node currentState;
+    [Export] private Node[] states;
+
+    public override void _Ready()
+    {
+        currentState.Notification(5001);
+
+    }
+
+    public void SwitchState<T>()
+    {
+        Node newState = null;
+        foreach (Node state in states)
+        {
+            if (state is T)
+            {
+                newState = state;
+                break;
+            }
+        }
+        if (newState == null)
+        {
+            GD.PrintErr("State not found");
+            return;
+        }
+        currentState = newState;
+        currentState.Notification(5001);
+    }
+}
